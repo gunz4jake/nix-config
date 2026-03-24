2555b72490a09d46f70d7644f267e21edf4c3a01
Jacob Karr <jacob@ningen.gg>
2026-03-24 15:28:31 -0400

    push

--- hosts/nixpad/default.nix
+++ hosts/nixpad/default.nix
@@ -25,7 +25,7 @@
   networking.quad9-dot.enable = true;
   custom.xanmod.enable = true;
   custom.plymouth.enable = true;
-  custom.desktop.environment = "plasma";
+  custom.desktop.environment = "gnome";
 
   # Use S3 deep sleep instead of s2idle — dramatically reduces battery drain on suspend.
   boot.kernelParams = [ "mem_sleep_default=deep" ];
